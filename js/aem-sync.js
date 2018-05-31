8797feece655723262904ea20471355482b8c3d1
kevinweber <k-iwi@web.de>
2018-05-31 15:37:41 +0200

    Make AEM Front work with aemsync v3

--- js/aem-sync.js
+++ js/aem-sync.js
@@ -9,7 +9,7 @@ const browserSync = require('./browser-sync.js');
 
 const aemsync = require('aemsync');
 const Watcher = aemsync.Watcher;
-const Pusher = aemsync.Pusher;
+const Pipeline = aemsync.Pipeline;
 
 const ANSI_COLOR_CYAN = '\x1b[36m';
 const ANSI_COLOR_RESET = '\x1b[0m';
@@ -25,6 +25,7 @@ Options:
   -w path_to_watch     Default is current
   -e exclude_filter    Anymach exclude filter; disabled by default
   -i sync_interval     Update interval in milliseconds; default is 100
+  -u packmgr_path      Package manager path; default is /crx/packmgr/service.jsp
   -o open_page         Browser page to be opened after successful launch; default is "false".
   -b browser           Browser where page should be opened in; this parameter is platform dependent; for example, Chrome is "google chrome" on OS X, "google-chrome" on Linux and "chrome" on Windows; default is "google chrome"
   -h                   Displays this screen
@@ -53,16 +54,22 @@ let init = () => {
     return;
   }
 
-  let workingDir = path.resolve(args.w || ".");
-  let targets = args.t || "http://admin:admin@localhost:4502";
-  let pushInterval = args.i || 100;
-  let exclude = args.e || "";
+  let workingDir = path.resolve(args.w || '.');
+  let targets = (args.t || 'http://admin:admin@localhost:4502').split(',')
+  let interval = args.i || 100;
+  let packmgrPath = args.u;
+  let exclude = args.e || '';
   let startPage = args.o || false;
-  let startBrowser = args.browser || "google chrome";
+  let startBrowser = args.browser || 'google chrome';
+
+  if (targets.length > 1) {
+    console.log(`You've provided multiple targets. Only the first one is used by BrowserSync.`);
+    return;
+  }
 
   browserSync.create({
-      name: 'aem-sync',
-      proxy: targets
+    name: 'aem-sync',
+    proxy: targets[0]
   });
 
   // Overview ANSI color codes: http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
@@ -72,39 +79,36 @@ let init = () => {
   }
 
   console.separate();
-  console.log("Working dir:", workingDir);
-  console.log("Targets:", targets);
-  console.log("Interval:", pushInterval);
-  console.log("Exclude:", exclude);
+  console.log('Working dir:', workingDir);
+  console.log('Targets:', targets);
+  console.log('Interval:', interval);
+  console.log('Exclude:', exclude);
   console.separate();
 
-  let pusher = new Pusher(targets.split(','), 600, reloadBrowser);
-  let watcher = new Watcher();
-
-  // Initialize queue processing
-  pusher.start();
-
-  // Watch over workingDir
-  watcher.watch(workingDir, exclude, (localPath) => {
-    // Add item to Pusher's queue when a change is detected
-    setTimeout(function () {
-      pusher.enqueue(localPath);
-    }, pushInterval);
+  // Reload browser once files are pushed to AEM instance
+  let onPushEnd = reloadBrowser;
+  aemsync({
+    workingDir,
+    targets,
+    interval,
+    exclude,
+    packmgrPath,
+    onPushEnd
   });
 
-  if (startPage !== false && startPage !== "false") {
+  if (startPage !== false && startPage !== 'false') {
     opn(startPage, {
       app: startBrowser
     });
   }
 
   console.separate();
-  console.log("NOTE: AEM Front can work together with the corresponding Chrome browser extension. To check if you're using the most up-to-date version, go to " + ANSI_COLOR_CYAN + "http://kevinw.de/aem-front-status/" + ANSI_COLOR_RESET + ". But you can also use AEM Front without extension by adding the code snippet displayed below into your website manually.");
+  console.log(`NOTE: AEM Front can work together with the corresponding Chrome browser extension. To check if you're using the most up-to-date version, go to ${ANSI_COLOR_CYAN}https://kevinw.de/aem-front-status/${ANSI_COLOR_RESET}. But you can also use AEM Front without extension by adding the code snippet displayed below into your website manually.`);
   console.separate();
-  console.log("\n");
+  console.log('\n');
 };
 
-if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   module.exports = {
     init: init
   };
