8dc760cd98da867b0d3a9d5b85a3147e831c231e
Kevin Weber <kevin.weber@airbnb.com>
2019-06-13 01:11:21 +0200

    Upgrade aemsync to v4

--- js/aem-sync.js
+++ js/aem-sync.js
@@ -1,20 +1,23 @@
-const path = require('path');
-const fs = require('graceful-fs');
-const opn = require('opn');
-const minimist = require('minimist');
+const path = require("path");
+const fs = require("graceful-fs");
+const opn = require("opn");
+const minimist = require("minimist");
 
-const packageInfo = require('./../package.json');
+const packageInfo = require("./../package.json");
 
-const browserSync = require('./browser-sync.js');
+const browserSync = require("./browser-sync.js");
 
-const aemsync = require('aemsync');
+const aemsync = require("aemsync");
+const aemsyncDefaults = require("aemsync/src/defaults");
+
 const Watcher = aemsync.Watcher;
 const Pipeline = aemsync.Pipeline;
 
-const ANSI_COLOR_CYAN = '\x1b[36m';
-const ANSI_COLOR_RESET = '\x1b[0m';
+const ANSI_COLOR_CYAN = "\x1b[36m";
+const ANSI_COLOR_MAGENTA = "\x1b[35m";
+const ANSI_COLOR_RESET = "\x1b[0m";
 
-console.separate = () => {
+const consoleSeparator = () => {
   console.log("---------------------------------------");
 };
 
@@ -32,19 +35,19 @@ Options:
   -v                   Displays version of this package`;
 
 let reloadBrowser = browserSyncTarget => (err, host) => {
-    if (err) {
-        return console.error(`Error when pushing package to ${host}`, err);
-    }
+  if (err) {
+    return console.error(`Error when pushing package to ${host}`, err);
+  }
 
-    if (browserSyncTarget.includes(host)) {
-        browserSync.reload({
-            name: 'aem-sync'
-        });
-    }
+  if (browserSyncTarget.includes(host)) {
+    browserSync.reload({
+      name: "aem-sync"
+    });
+  }
 };
 
 let init = () => {
-  'use strict';
+  "use strict";
 
   let args = minimist(process.argv.slice(2));
 
@@ -60,40 +63,57 @@ let init = () => {
     return;
   }
 
-  let workingDir = path.resolve(args.w || '.');
-  let targets = (args.t || 'http://admin:admin@localhost:4502').split(',');
-  let browserSyncTarget = targets[0];
-  let interval = args.i || 100;
-  let packmgrPath = args.u;
-  let exclude = args.e || '';
-  let startPage = args.o || false;
-  let startBrowser = args.browser || 'google chrome';
+  const workingDir = path.resolve(args.w || aemsyncDefaults.workingDir);
+  const targets = args.t
+    ? typeof args.t === "string"
+      ? [...args.t.split(",")]
+      : args.t
+    : aemsyncDefaults.targets;
+  const browserSyncTarget = targets[0];
+  const interval = args.i || 100;
+  const packmgrPath = args.u || aemsyncDefaults.packmgrPath;
+  const exclude = args.e
+    ? typeof args.e === "string"
+      ? [...args.e.split(",")]
+      : args.e
+    : aemsyncDefaults.exclude;
+
+  const startPage = args.o || false;
+  const startBrowser = args.browser || "google chrome";
 
   if (targets.length > 1) {
-    console.log(`You've provided multiple targets. Only the first one is used by BrowserSync.`);
+    consoleSeparator();
+    console.log(
+      `You've provided multiple targets. BrowserSync only uses the first one: ${browserSyncTarget}`
+    );
   }
 
   browserSync.create({
-    name: 'aem-sync',
+    name: "aem-sync",
     proxy: browserSyncTarget
   });
 
-  // Overview ANSI color codes: http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+  // Overview of ANSI color codes: http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
   if (!fs.existsSync(workingDir)) {
-    console.log(ANSI_COLOR_CYAN, 'Invalid path:', workingDir, ANSI_COLOR_RESET);
+    consoleSeparator();
+    console.log(
+      `Invalid path: ${ANSI_COLOR_CYAN}${workingDir}${ANSI_COLOR_RESET}`
+    );
     return;
   }
 
-  console.separate();
-  console.log('Working dir:', workingDir);
-  console.log('Targets:', targets);
-  console.log('Interval:', interval);
-  console.log('Exclude:', exclude);
-  console.separate();
+  consoleSeparator();
+  console.log(`
+  Watch over: ${ANSI_COLOR_MAGENTA}${workingDir}${ANSI_COLOR_RESET}
+     Targets: ${ANSI_COLOR_MAGENTA}${targets}${ANSI_COLOR_RESET}
+    Excludes: ${ANSI_COLOR_MAGENTA}${exclude}${ANSI_COLOR_RESET}
+    Interval: ${ANSI_COLOR_MAGENTA}${interval}${ANSI_COLOR_RESET}
+  `);
 
   // Reload browser once files are pushed to AEM instance
   let onPushEnd = reloadBrowser(browserSyncTarget);
-  aemsync({
+
+  aemsync(workingDir, {
     workingDir,
     targets,
     interval,
@@ -102,19 +122,21 @@ let init = () => {
     onPushEnd
   });
 
-  if (startPage !== false && startPage !== 'false') {
+  if (startPage !== false && startPage !== "false") {
     opn(startPage, {
       app: startBrowser
     });
   }
 
-  console.separate();
-  console.log(`NOTE: It's recommended to use AEM Front together with the corresponding Chrome browser extension. Get it from the official app store: ${ANSI_COLOR_CYAN}https://chrome.google.com/webstore/detail/cmpbphecgagbhhociicpakhddeagjlih${ANSI_COLOR_RESET}. Alternatively, you can use AEM Front without extension by adding a BrowserSync code into your AEM site manually.`);
-  console.separate();
-  console.log('\n');
+  consoleSeparator();
+  console.log(
+    `NOTE: It's recommended to use AEM Front together with the corresponding Chrome browser extension. Get it from the official app store: ${ANSI_COLOR_CYAN}https://chrome.google.com/webstore/detail/cmpbphecgagbhhociicpakhddeagjlih${ANSI_COLOR_RESET}. Alternatively, you can use AEM Front without extension by adding a BrowserSync code into your AEM site manually.`
+  );
+  consoleSeparator();
+  console.log("\n");
 };
 
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
   module.exports = {
     init: init
   };
